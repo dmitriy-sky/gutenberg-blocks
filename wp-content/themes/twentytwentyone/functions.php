f0a63ee79feef7b0c0bf5650fb112d736fe4c996
Dmitriy Sky <dmitriy.sky@speedandfunction.com>
2021-02-10 14:09:59 +0300

    Create ACF Block example in plugin

--- wp-content/themes/twentytwentyone/functions.php
+++ wp-content/themes/twentytwentyone/functions.php
@@ -635,32 +635,32 @@ add_action( 'wp_footer', 'twentytwentyone_add_ie_class' );
  *
  */
 
-add_action('acf/init', 'my_acf_init');
-function my_acf_init() {
+// add_action('acf/init', 'my_acf_init');
+// function my_acf_init() {
 
-	// check function exists
-	if( function_exists('acf_register_block') ) {
+// 	// check function exists
+// 	if( function_exists('acf_register_block') ) {
 
-		// register a testimonial block
-		acf_register_block(array(
-			'name'				=> 'testimonial',
-			'title'				=> __('Testimonial'),
-			'description'		=> __('A custom testimonial block.'),
-			'render_callback'	=> 'my_acf_block_render_callback',
-			'category'			=> 'formatting',
-			'icon'				=> 'admin-comments',
-			'keywords'			=> array( 'testimonial', 'quote' ),
-		));
-	}
-}
+// 		// register a testimonial block
+// 		acf_register_block(array(
+// 			'name'				=> 'testimonial',
+// 			'title'				=> __('Testimonial'),
+// 			'description'		=> __('A custom testimonial block.'),
+// 			'render_callback'	=> 'my_acf_block_render_callback',
+// 			'category'			=> 'formatting',
+// 			'icon'				=> 'admin-comments',
+// 			'keywords'			=> array( 'testimonial', 'quote' ),
+// 		));
+// 	}
+// }
 
-function my_acf_block_render_callback( $block ) {
+// function my_acf_block_render_callback( $block ) {
 
-	// convert name ("acf/testimonial") into path friendly slug ("testimonial")
-	$slug = str_replace('acf/', '', $block['name']);
+// 	// convert name ("acf/testimonial") into path friendly slug ("testimonial")
+// 	$slug = str_replace('acf/', '', $block['name']);
 
-	// include a template part from within the "template-parts/block" folder
-	if( file_exists( get_theme_file_path("/template-parts/block/content-{$slug}.php") ) ) {
-		include( get_theme_file_path("/template-parts/block/content-{$slug}.php") );
-	}
-}
+// 	// include a template part from within the "template-parts/block" folder
+// 	if( file_exists( get_theme_file_path("/template-parts/block/content-{$slug}.php") ) ) {
+// 		include( get_theme_file_path("/template-parts/block/content-{$slug}.php") );
+// 	}
+// }
